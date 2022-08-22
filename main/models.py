a1606d528fa179fcdf8ca124a0a3c14e674b4951
Jacob Peat <jacob.peat01@gmail.com>
2022-08-22 11:24:51 +0100

    Create model for task

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200, )
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created_time_and_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}, created on {self.created_time_and_date}"
+
+    class Meta:
+        ordering = ['complete']
+    
