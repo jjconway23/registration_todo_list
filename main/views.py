0d7af4553fecdda0049b178da0c9b571c913bb92
Jacob Peat <jacob.peat01@gmail.com>
2022-08-22 15:25:52 +0100

    Create view for updating a task

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from asyncio import tasks
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from .models import Task
@@ -20,3 +20,9 @@ class TaskCreate(CreateView):
      model = Task
      fields = '__all__'
      success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '_all_'
+    success_url = reverse_lazy('tasks')
